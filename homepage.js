1851547e1953784765e836c7cc1071c8a3919370
ZhenfangChen <zc8hs@virginia.edu>
2019-11-21 11:51:30 -0500

    Added Content

---
+++ homepage.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+
+		$("#more-species").click(function() {
+
+			for (i=0; i<8; i++) {
+				$(".card-container").append('<div class="col-xs-12 col-sm-6 col-md-4 col-lg-3"><div class="card"><img src="assets/img/cat.jpg" class="card-img-top" alt="A picture of a cute, gray and white cat with yellow eyes." title="A picture of a cute, gray and white cat with yellow eyes."><div class="card-body"><h5 class="card-title">I\'m a cat!</h5><p class="card-text">Some quick example text to build on the card title and make up the bulk of the card\'s content.</p><button class="btn btn-primary">Hide Me</button></div></div>');
+			}
+
+			clickToHide();
+		});
+	
+});
---
+++ map.js
@@ -0,0 +1,10 @@
+mapboxgl.accessToken = 'pk.eyJ1IjoiemhlbmZhbmciLCJhIjoiY2pjdjR0NXZqMG8ycTJxb2Z3czB6Z2dtcSJ9.-rPyXSJ6XQ_bp7W4ytuqkw';
+
+var map = new mapboxgl.Map({
+	container: 'map',
+	minZoom: 10,
+	maxZoom: 17,
+	style: 'mapbox://styles/zhenfang/ck24u74by0ni61cqvrrs213zy'
+	
+
+});
